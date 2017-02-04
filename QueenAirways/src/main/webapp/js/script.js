7809eaeaa49785751edc6e8c394d5961cb71b61a
omarlm1992@gmail.com <omarl@192.168.1.39>
2017-02-04 18:36:24 +0000

    Haciendo jquery y validaciones

--- QueenAirways/src/main/webapp/js/script.js
+++ QueenAirways/src/main/webapp/js/script.js
@@ -1,16 +1,6 @@
-$(document).ready(function () {
-	//$('#header').load("include/header.html");
-	$(".button-collapse").sideNav();
-	$('.parallax').parallax();
-
-
-
-	var sliderFormat = document.getElementById('rangePrice');
-
-	$("#rangePrice").ionRangeSlider({
-		min: 0,
-		max: 2000,
-		from: 550,
-		prefix: '€'
-	});
-});
+$(document).ready(function(){
+    $('#btnSearch').click(function(e){
+        $(".div_page1").parent().hide('slow');
+        $(".div_page2").show('slow');
+    })
+})
