f1297a4d32460e85eb182188f5b2d0f082a883c1
OmarLM <OmarLM@10.2.6.37>
2017-02-05 14:22:11 +0000

    Añadido cambios de div central, menos la última parte

--- QueenAirways/src/main/webapp/js/script.js
+++ QueenAirways/src/main/webapp/js/script.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     $('#btnSearch').click(function(e){
         $(".div_page1").parent().hide('slow');
         $(".div_page2").show('slow');
-    })
-})
+    });
+
+    $('.row_page_2').children().click(function(e){
+        $(".row_page_2").hide('slow');
+        $(".div_page3").show('slow');
+    });
+});
