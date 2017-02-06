18afb8836a2dab2419045d6af8f36965d1cf9408
omarlm1992@gmail.com <omarl@192.168.1.39>
2017-02-06 19:05:37 +0000

    Añado timepicker, se corrige lo del dichoso icono, se modifica el tamaño de las imagenes y se cambia el nombre del Avión.html a avion.html

--- QueenAirways/src/main/webapp/js/design.js
+++ QueenAirways/src/main/webapp/js/design.js
@@ -1,16 +1,22 @@
 $(document).ready(function () {
-	//$('#header').load("include/header.html");
-	$(".button-collapse").sideNav();
-	$('.parallax').parallax();
+    //$('#header').load("include/header.html");
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
 
+    $('.timepicker').pickatime({
+        default: 'now',
+        twelvehour: false,
+        donetext: 'OK',
+        autoclose: false,
+        vibrate: true
+    });
 
+    var sliderFormat = document.getElementById('rangePrice');
 
-	var sliderFormat = document.getElementById('rangePrice');
-
-	$("#rangePrice").ionRangeSlider({
-		min: 0,
-		max: 2000,
-		from: 550,
-		prefix: '€'
-	});
+    $("#rangePrice").ionRangeSlider({
+        min: 0,
+        max: 2000,
+        from: 550,
+        prefix: '€'
+    });
 });
